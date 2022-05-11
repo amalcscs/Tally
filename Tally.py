ae89763711d0bd6950f4c4d56895a2ae100f9b47
amalcscs <97750974+amalcscs@users.noreply.github.com>
2022-05-11 15:11:49 +0000

    create company frontend page and navigation menus

--- Tally.py
+++ Tally.py
@@ -1,16 +1,63 @@
-# importing tkinter gui
+from tkinter import *
 import tkinter as tk
- 
-#creating window
-window=tk.Tk()
- 
-#getting screen width and height of display
-width= window.winfo_screenwidth()
-height= window.winfo_screenheight()
-#setting tkinter window size
-window.geometry("%dx%d" % (width, height))
-window.title("Tally")
-label = tk.Label(window, text="Hello Tkinter!")
-label.pack()
- 
-window.mainloop()
+from tkinter import messagebox
+from PIL import Image,ImageTk
+from tkinter.ttk import Combobox
+
+from tkinter import ttk
+
+
+
+def account():
+    global Canvas1
+    Canvas1 = tk.Canvas( background="#B0B0B0", insertbackground="black", relief="ridge",
+                                selectbackground="blue", selectforeground="white")
+    Canvas1.place(relx=0, rely=0.10, relheight=0.800, relwidth=.830)
+
+    global Canvas2
+    Canvas2 = tk.Canvas(Canvas1, background="#ffffff", insertbackground="black", relief="ridge",
+                            selectbackground="blue", selectforeground="white")
+    Canvas2.place(relx=0.15, rely=0.105, relheight=0.8, relwidth=0.700)
+
+    global Canvas3
+    Canvas3 = tk.Canvas(background=" #e6faf", insertbackground="black", relief="ridge",
+                                selectbackground="blue", selectforeground="white")
+    Canvas3.place(relx=0.830, rely=0.100, relheight=0.8, relwidth=0.150)
+
+    
+
+
+global screen
+screen=Tk()
+w=screen.winfo_screenwidth()
+h=screen.winfo_screenheight()
+screen.geometry("%dx%d" %(w,h))
+        
+screen.title("Welcome")
+p1 = PhotoImage(file='C:\\Users\\Screenshot.png')
+screen.iconphoto(True, p1)
+screen.configure(background="#848884")
+screen.configure(cursor="arrow")
+          
+sbmibtn=Button(screen,text='K:Company',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=account).place(x=20,y=10)
+sbmibtn=Button(screen,text='Y:Data',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=search_account).place(x=180,y=10)
+sbmibtn=Button(screen,text='Z:Exchange',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=deposit).place(x=340,y=10)
+sbmibtn=Button(screen,text='G:Go To',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=withdraw).place(x=600,y=10)
+sbmibtn=Button(screen,text='O:Import',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=transactions).place(x=850,y=10)
+sbmibtn=Button(screen,text='E;Export',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=update).place(x=990,y=10)
+sbmibtn=Button(screen,text='M:E-mail',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=balance).place(x=1130,y=10)
+sbmibtn=Button(screen,text='P:Print',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=customer_list).place(x=1270,y=10)
+sbmibtn=Button(screen,text='F1:Help',borderwidth="0",background="#023047",
+                                     foreground="white",width=10,font="-family {Segoe UI} -size 12 -weight bold ",command=login).place(x=1400,y=10)
+
+
+screen.mainloop()
+
